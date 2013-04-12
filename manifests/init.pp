5539a17351b3cb3b08c8b9c3d464930036077576
Thomas Van Doren <thomas.vandoren@gmail.com>
2013-04-12 15:36:17 -0700

    Fix lint warnings.

--- manifests/init.pp
+++ manifests/init.pp
@@ -25,18 +25,18 @@
 #
 class etckeeper {
   # HIGHLEVEL_PACKAGE_MANAGER config setting.
-  $etckeeper_high_pkg_mgr = $operatingsystem ? {
+  $etckeeper_high_pkg_mgr = $::operatingsystem ? {
     /(?i-mx:ubuntu|debian)/                           => 'apt',
     /(?i-mx:centos|fedora|redhat|oraclelinux|amazon)/ => 'yum',
   }
 
   # LOWLEVEL_PACKAGE_MANAGER config setting.
-  $etckeeper_low_pkg_mgr = $operatingsystem ? {
+  $etckeeper_low_pkg_mgr = $::operatingsystem ? {
     /(?i-mx:ubuntu|debian)/                           => 'dpkg',
     /(?i-mx:centos|fedora|redhat|oraclelinux|amazon)/ => 'rpm',
   }
 
-  $gitpackage = $operatingsystem ? {
+  $gitpackage = $::operatingsystem ? {
     /(?i-mx:ubuntu|debian)/                           => 'git-core',
     /(?i-mx:centos|fedora|redhat|oraclelinux|amazon)/ => 'git',
   }
